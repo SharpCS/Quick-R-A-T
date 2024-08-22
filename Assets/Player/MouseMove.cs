8e656efcc9f53d4b971471abf9f3ad578714a8c6
sharpcustomer53 <sharpcustomer53@gmail.com>
2024-08-22 11:34:50 +0900

    プロジェクト作成

---
+++ Assets/Player/MouseMove.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+    Vector3 mousePos, pos; //マウスとゲームオブジェクトの座標データを格納
+    void Update()
+    {
+        //マウスの座標を取得する
+        mousePos = Input.mousePosition;
+        //スクリーン座標をワールド座標に変換する
+        pos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
+        //ワールド座標をゲームオブジェクトの座標に設定する
+        transform.position = pos;
+    }
+}
